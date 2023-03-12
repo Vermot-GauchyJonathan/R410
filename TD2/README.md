859e19b24ad18b0087eaef7b5aeb0f1b2092be36
Jonathan Vermot-Gauchy <jonathan.vermot-gauchy@etu.univ-cotedazur.fr>
2023-03-12 12:04:33 +0100

    fin exercice 1

--- TD2/README.md
+++ TD2/README.md
@@ -1,1 +1,21 @@
 # M413 - TD2 : Réponses aux Questions
+
+# Exercice 1
+## 6.1  
+### Comment avez-vous ajouté l'écouteur d'évènement et sur quel objet ?
+    document.getElementsByTagName("body")[0].addEventListener('click',initSelect);
+    
+    J ai selectionné le body et apres j ai ajouté un listener dessus.
+
+### Que se passe-t-il si vous utilisez currentTarget en lieu et place de target ?
+    currentTarget applique l effet sur l element qui a le listener 
+    target applique l effet sur l element qu on click
+
+## 6.2
+### Comment avez-vous ajouté l’élément ?
+    Avec la fonction insertBefore de js
+
+### Comment avez-vous fait pour que la fonction select2() ignore les évèments de la <div> donnée ci-dessus ?
+    J ai fait une verification de l id de l element cible par select2
+
+# Exercice 2
--- TD2/js/td2.js
+++ TD2/js/td2.js
@@ -11,7 +11,7 @@
 "use strict";
 
 // M413 - TD2
-let message = 'JavaScript is ok :)';
+var message = 'JavaScript is ok :)';
 // alert( message);
 console.log( message);
 
@@ -22,9 +22,120 @@ function onLoad() {
 // Toute les ressources de la page sont complètement chargées.
 window.onload = onLoad;
 
+var selected2 = null;
 
-function initSelect()
+
+
+function initSelect(clic)
 {
-	//change la classe de l element selectionne pour lui mettre un fond rouge
+	const elementClick = clic.target;
+	var classes = elementClick.classList;
+	classes.add("select");
+
+}
+/*
+function select(clic)
+{
+	const elementClick = clic.currentTarget;
+	var classes = elementClick.classList;
+	if (classes.contains("select"))
+	{
+		classes.remove("select");
+	}
+	else
+	{
+		classes.add("select");
+	}
+}
+ */
+
+function select(clic)
+{
+	const elementClick = clic.target;
+	var classes = elementClick.classList;
+	if (classes.contains("select"))
+	{
+		classes.remove("select");
+	}
+	else
+	{
+		classes.add("select");
+	}
 }
 
+
+function insert()
+{
+	var monbody = document.getElementsByTagName("body");
+	var madiv = document.createElement("div");
+	madiv.id = "insert-div";
+
+	var monselect = document.createElement("select");
+	monselect.id = "insert-type";
+	monselect.name = "type";
+	monselect.options[0] = new Option("div","div");
+	monselect.options[1] = new Option("p","p");
+	monselect.options[2] = new Option("span","span");
+
+
+	var moninput = document.createElement("input");
+	moninput.id = "insert-text";
+	moninput.type = "text";
+	moninput.value = "My New Text Element";
+
+	madiv.appendChild(monselect);
+	madiv.appendChild(moninput);
+	document.body.insertBefore(madiv,document.body.firstChild);
+}
+
+function select2(clic)
+{
+
+	if (clic.target.parentElement.id != "insert-div")
+	{
+		var clicked = clic.target;
+		if (selected2 != null)
+		{
+			if (clicked.classList.contains("select2"))
+			{
+				clicked.classList.remove("select2");
+			}
+			else
+			{
+				selected2.classList.remove("select2");
+				clicked.classList.add("select2");
+				selected2 = clicked;
+			}
+		}
+		else
+		{
+			clicked.classList.add("select2");
+			selected2 = clicked;
+		}
+	}
+}
+
+function insertElement(target)
+{
+	if(selected2 != null)
+	{
+		var type = document.getElementById("insert-type");
+		var text = document.getElementById("insert-text");
+		var monelement = document.createElement(type.value);
+		monelement.textContent = text.value;
+
+
+		target.parentNode.insertBefore(monelement,target);
+	}
+}
+
+insert();
+//document.getElementsByTagName("body")[0].addEventListener('click',initSelect);
+//document.getElementsByTagName("body")[0].addEventListener('click',select);
+document.getElementsByTagName("body")[0].addEventListener('click',select2);
+document.getElementsByTagName("body")[0].addEventListener('click', function (e) {
+	if (selected2 != null) {
+		e.stopPropagation();
+		insertElement(selected2);
+	}
+});
