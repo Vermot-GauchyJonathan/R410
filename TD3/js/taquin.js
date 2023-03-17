7bbf84d6582b90d5b17ab0d45a48f692d7e65ec8
Jonathan Vermot-Gauchy <jonathan.vermot-gauchy@etu.univ-cotedazur.fr>
2023-03-17 09:45:58 +0100

    taquin avancement

--- TD3/js/taquin.js
+++ TD3/js/taquin.js
@@ -24,3 +24,61 @@ function onLoad() {
 }
 // Toute les ressources de la page sont complètement chargées.
 window.onload = onLoad;
+
+
+var divgame = document.getElementById("game");
+var cases = divgame.getElementsByTagName("div");
+var a = 0;
+console.log(cases);
+var tabCases = new Array(3);
+for (var ligne = 0; ligne<3;ligne++)
+{
+	tabCases[ligne] = new Array(3);
+	for (var colone = 0; colone<3; colone++)
+	{
+		tabCases[ligne][colone] = cases[a];
+		a++;
+	}
+}
+
+//console.log(tabCases);
+
+
+function selection(event)
+{
+	//recuperation de l ement clique
+	var cible = event.target
+	if(cible.tagName == "SPAN")
+	{
+		console.log("ok");
+		cible = cible.parentElement;
+	}
+	//console.log(cible);
+
+	//recherche de la place de la cellule clique
+	for (var i =0;i<3; i++)
+	{
+		for (var j = 0;j<3;j++)
+		{
+			if (tabCases[i][j].id == cible.id)
+			{
+				console.log("identique ["+i+"]"+"["+j+"]");
+			}
+		}
+
+	}
+
+}
+
+
+
+
+
+
+
+for (var i =0;i<cases.length; i++)
+{
+	cases[i].addEventListener('click',selection);
+}
+
+
