55d0bb0c3a3910ebc1142d7299e6502e23c3a294
Jonathan Vermot-Gauchy <jonathan.vermot-gauchy@etu.univ-cotedazur.fr>
2023-03-24 08:15:44 +0100

    ajout TP3bis

---
+++ TP3bis/js/2048.js
@@ -0,0 +1,26 @@
+/**
+* 
+* M413 - TD3 - 2048 Game
+* * 
+* @author Jean-Michel Bruneau
+*	@copyright UCA - IUT -INFO
+* @version	1.0
+* @date			2021-01-31
+*
+*/
+"use strict";
+
+// M413 - Taquin
+let message = 'JavaScript is ok :)';
+// alert( message);
+console.log(message);
+
+function onLoad() {
+	console.log('Processus de chargement du document terminé…');
+	//
+	// All your JavaScript code goes here !
+	//
+ 
+}
+// Toute les ressources de la page sont complètement chargées.
+window.onload = onLoad;
